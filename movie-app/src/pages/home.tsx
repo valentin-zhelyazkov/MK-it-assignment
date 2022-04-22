65b8c40e033068e5fc3c9e28c772ea7b1cc81278
ValentinZhelyazkov <valentin.zhelyazkov@pytek.bg>
2022-04-22 15:22:05 +0300

    hero

--- movie-app/src/pages/home.tsx
+++ movie-app/src/pages/home.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { db } from '../database/db';
 import { collection, getDocs } from "firebase/firestore";
-import { Button } from '@mui/material';
+import { Box, Button } from '@mui/material';
 
 const Home = () => {
     const test = async () => {
@@ -11,10 +11,24 @@ const Home = () => {
     }
 
     return (
-    <div>
-        <h1 className="text-3xl font-bold underline">dsadsa</h1>
-        <Button onClick={test} variant="outlined">Fetch</Button>
-    </div>
+        <>
+            <Box
+                component="img"
+                className="w-screen h-96 absolute -z-1"
+                alt="The house from the offer."
+                src="https://t4.ftcdn.net/jpg/03/98/67/05/360_F_398670578_qfihCy61VOBGpuvWolQ8U87H2cqmeJ8L.jpg"
+            />
+            <Box
+                className="w-screen h-96 absolute"
+            >
+                <div className="flex flex-col justify-center h-full w-1/3 pl-12">
+                    <h1 className="text-white font-extrabold text-2xl">Seach your favourite movies</h1>
+                    <p className="text-white py-6">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Quae, est!</p>
+                    <Button variant="contained" className="w-24">Search</Button>
+                </div>
+            </Box>
+            <p>sgefa</p>
+        </>
     );
 }
 
